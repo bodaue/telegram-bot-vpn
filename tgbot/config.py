a15503efbfda1d55e96c02a1b92c03d84d9f1a63
Timur Kerimov <tim-online@mail.ru>
2024-07-11 11:01:51 +0300

    Migrate dataclass config to pydantic_settings

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,53 +1,42 @@
-from dataclasses import dataclass
-
-from environs import Env
-
-
-@dataclass
-class DbConfig:
-    host: str
-    name: str
-    port: int = 27017
+from pydantic import SecretStr, BaseModel
+from pydantic_settings import BaseSettings as _BaseSettings, SettingsConfigDict
 
 
-@dataclass
-class TgBot:
-    token: str
-    admin_ids: list[int]
-    use_mongo_storage: bool
-
-
-@dataclass
-class Miscellaneous:
-    yoomoney_token: str
-    yoomoney_wallet: str
-
-
-@dataclass
-class Config:
-    tg_bot: TgBot
-    misc: Miscellaneous = None
-    db: DbConfig = None
-
-
-def load_config(path: str = '.env') -> Config:
-    env = Env()
-    env.read_env(path)
-
-    return Config(
-        tg_bot=TgBot(
-            token=env.str("BOT_TOKEN"),
-            admin_ids=env.list('ADMINS', subcast=int),
-            use_mongo_storage=env.bool("USE_MONGO_STORAGE"),
-
-        ),
-        db=DbConfig(host=env.str('DB_HOST'),
-                    port=env.int('DB_PORT'),
-                    name=env.str('DB_NAME')),
-
-        misc=Miscellaneous(yoomoney_token=env.str('YOOMONEY_TOKEN'),
-                           yoomoney_wallet=env.str('YOOMONEY_WALLET'))
+class BaseSettings(_BaseSettings):
+    model_config = SettingsConfigDict(
+        extra="ignore", env_file=".env", env_file_encoding="utf-8"
     )
 
 
-config = load_config('.env')
+class CommonConfig(BaseSettings, env_prefix="COMMON_"):
+    bot_token: SecretStr
+    admins: list[int]
+    use_mongo_storage: bool = False
+
+
+class DbConfig(BaseSettings, env_prefix="DB_"):
+    host: str
+    port: int
+    name: str
+
+
+class YoomoneyConfig(BaseSettings, env_prefix="YOOMONEY_"):
+    token: SecretStr
+    wallet: str
+
+
+class Config(BaseModel):
+    common: CommonConfig
+    db: DbConfig
+    yoomoney: YoomoneyConfig
+
+
+def create_app_config() -> Config:
+    return Config(
+        common=CommonConfig(),
+        db=DbConfig(),
+        yoomoney=YoomoneyConfig(),
+    )
+
+
+config = create_app_config()
